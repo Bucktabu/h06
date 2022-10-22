fc2a8fd16f261eda3d4043ffafe4c5327b6e7a0d
Bucktabu <stanislaworonin@yandex.ru>
2022-10-22 12:43:29 +0300

    fix return token in header

--- src/middlewares/auth-middleware.ts
+++ src/middlewares/auth-middleware.ts
@@ -3,17 +3,11 @@ import {jwsService} from "../application/jws-service";
 import {usersService} from "../domain/user-service";
 
 export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
-    // if (!req.headers.authorization) {
-    //     return res.sendStatus(401)
-    // }
-
-    if (!req.body.accessToken) {
+    if (!req.headers.authorization) {
         return res.sendStatus(401)
     }
 
-    //const token = req.headers.authorization.split(' ')[1]
-
-    const token = req.body.accessToken.split(' ')[1]
+    const token = req.headers.authorization.split(' ')[1]
     const userId = await jwsService.getUserIdByToken(token)
 
     if (!userId) {
