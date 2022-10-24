a18be6e3e2f99d013f038275e91e41aba377be94
Stanislav <stanislaworonin@yandex.ru>
2022-10-24 14:01:55 +0300

    refactoring usersRouterMiddleware

--- src/middlewares/validation-middleware/userRouter-validation.ts
+++ src/middlewares/validation-middleware/userRouter-validation.ts
@@ -1,8 +1,7 @@
 import {body} from "express-validator";
-import {inputValidation} from "./input-validation";
 
 const loginValidation = body('login').isString().trim().isLength({min: 3, max: 10})
 const passwordValidation = body('password').isString().trim().isLength({min: 6, max: 20})
 const emailValidation = body('email').isString().trim().notEmpty().isEmail()
 
-export const userRouterValidation = [loginValidation, passwordValidation, emailValidation, inputValidation]
+export const bodyUsersRouterValidation = [loginValidation, passwordValidation, emailValidation]
--- src/routers/users-router.ts
+++ src/routers/users-router.ts
@@ -1,9 +1,5 @@
 import {Response, Router} from "express";
 
-import {authenticationGuard} from "../middlewares/validation-middleware/authentication-guard";
-import {userRouterValidation} from "../middlewares/validation-middleware/userRouter-validation";
-import {usersQueryValidationMiddleware} from "../middlewares/validation-middleware/query-validation";
-
 import {usersService} from "../domain/user-service";
 
 import {CreateNewUser} from "../models/createNewUser";
@@ -12,13 +8,15 @@ import {URIParameters} from "../models/URIParameters";
 
 import {ContentPageType} from "../types/content-page-type";
 import {RequestWithBody, RequestWithParams, RequestWithQuery} from "../types/request-types";
+import {deleteUsersRouter,
+        getUsersRouterMiddleware,
+        postUsersRouterMiddleware} from "../middlewares/usersRouter-middleware";
 
 export const usersRouter = Router({})
 
 
 usersRouter.post('/',
-    authenticationGuard,
-    userRouterValidation,
+    postUsersRouterMiddleware,
     async (req: RequestWithBody<CreateNewUser>, res: Response) => {
 
         const newUser = await usersService.createNewUser(req.body.login, req.body.password, req.body.email)
@@ -31,8 +29,9 @@ usersRouter.post('/',
     })
 
 usersRouter.get('/',
-    ...usersQueryValidationMiddleware,
+    ...getUsersRouterMiddleware,
     async (req: RequestWithQuery<QueryParameters>, res: Response) => {
+
         const pageWithUsers: ContentPageType = await usersService
             .giveUsersPage(req.query.sortBy,
                            req.query.sortDirection,
@@ -49,7 +48,7 @@ usersRouter.get('/',
     })
 
 usersRouter.delete('/:id', // userId
-    authenticationGuard,
+    deleteUsersRouter,
     async (req: RequestWithParams<URIParameters>, res: Response) => {
 
         const isDeleted = await usersService.deleteUserById(req.params.id)
